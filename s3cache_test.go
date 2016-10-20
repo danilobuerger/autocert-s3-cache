1b7b9840f4e823181ded8279116edf21fea045e8
Danilo Bürger <info@danilobuerger.de>
2016-10-20 19:50:06 +0200

    Added cache test

--- s3cache_test.go
+++ s3cache_test.go
@@ -3,9 +3,17 @@
 package s3cache
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/acme/autocert"
+	"golang.org/x/net/context"
 )
 
 type testLogger struct {
@@ -27,4 +35,55 @@ func TestLogger(t *testing.T) {
 	assert.False(t, l.called)
 	c.log("")
 	assert.True(t, l.called)
+}
+
+type testS3 struct {
+	s3iface.S3API
+	cache map[string][]byte
+}
+
+func (t *testS3) GetObject(input *s3.GetObjectInput) (*s3.GetObjectOutput, error) {
+	b, ok := t.cache[*input.Key]
+	if !ok {
+		return nil, awserr.NewRequestFailure(nil, http.StatusNotFound, "")
+	}
+
+	return &s3.GetObjectOutput{
+		Body: ioutil.NopCloser(bytes.NewReader(b)),
+	}, nil
+}
+
+func (t *testS3) PutObject(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+	b, err := ioutil.ReadAll(input.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	t.cache[*input.Key] = b
+	return &s3.PutObjectOutput{}, nil
+}
+
+func (t *testS3) DeleteObject(input *s3.DeleteObjectInput) (*s3.DeleteObjectOutput, error) {
+	delete(t.cache, *input.Key)
+	return &s3.DeleteObjectOutput{}, nil
+}
+
+func TestCache(t *testing.T) {
+	cache := &Cache{s3: &testS3{cache: map[string][]byte{}}}
+	ctx := context.Background()
+
+	_, err := cache.Get(ctx, "nonexistent")
+	assert.Equal(t, autocert.ErrCacheMiss, err)
+
+	b1 := []byte{1}
+	assert.NoError(t, cache.Put(ctx, "dummy", b1))
+
+	b2, err := cache.Get(ctx, "dummy")
+	assert.NoError(t, err)
+	assert.Equal(t, b1, b2)
+
+	assert.NoError(t, cache.Delete(ctx, "dummy"))
+
+	_, err = cache.Get(ctx, "dummy")
+	assert.Equal(t, autocert.ErrCacheMiss, err)
 }
